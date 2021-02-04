96fcc240b53a638c14cf54a2eb4833e4eaeb8e87
Glitch (hello-webpage) <none>
2021-02-04 13:48:46 +0000

    🚅💵 Checkpoint ./style.css:10743404/3147 ./index.html:10743404/801 ./script.js:10743404/607

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+
+
+      
+
+const groceryList = document.getElementById("grocery-list");
+
+const submitItem = document.getElementById('toBuy').value = '';
+const createElem = document.createElement("p");
+createElem.innerHTML = submitItem;
+groceryList.appendChild(createElem);
