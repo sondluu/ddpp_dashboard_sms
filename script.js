a08984162166afd027d94e3c42130fa8821f3330
Glitch (hello-webpage) <none>
2021-02-04 18:35:37 +0000

    🌨🚣 Checkpoint ./index.html:10743404/520 ./script.js:10743404/470

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-// console.log("hi");
-
-
-// function submitItem(){
-//   var submittedItem = document.getElementById('toBuy').value;
-//   const createElem = document.createElement("p");
-//   createElem.innerHTML = submittedItem;
-//   groceryList.appendChild(createElem);
-// }
-      
-// const groceryList = document.getElementById("grocery-list");
 
